af62c8089673d8acd93b33e8b18e2af685231639
dededed6 <dededed338@gmail.com>
2019-03-14 22:12:17 +0900

    first commit

---
+++ Assets/Audio/Bounce.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    AudioSource ballAudio;
+    void Start()
+    {
+        ballAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ballAudio.Play();
+    }
+}
